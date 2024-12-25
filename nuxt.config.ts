7289482c95d614d8ab4efadfa70a35aca172ed28
KishanThakrar <kishant2012@gmail.com>
2024-12-25 18:12:25 +0530

    optimize images

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
+  ssr: true,
+  nitro: {
+    compressPublicAssets: true,
+    minify: true
+},
   devtools: { enabled: true },
   css: ['~/assets/css/style.css'],
   image: {    
