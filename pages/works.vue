ba22d77130fc5fbbf9adfac0b7f8a7d678da027a
KishanThakrar <kishant2012@gmail.com>
2025-01-01 22:08:48 +0530

    add: lazy

--- pages/works.vue
+++ pages/works.vue
@@ -41,10 +41,10 @@
                   class="w-full opacity-50"
                   :src="i.image"
                   :alt="i.name"
-                  loading="lazy"
-                  format="webp"
                   height="1330"
                   width="851"
+                  loading="lazy"
+                  format="webp"
                 />
 
                 <NuxtLink
