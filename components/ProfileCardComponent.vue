1254a51efc3f36b86b671c82ef79d5276349a7df
KishanThakrar <kishant2012@gmail.com>
2025-01-03 22:16:37 +0530

    add: sizes

--- components/ProfileCardComponent.vue
+++ components/ProfileCardComponent.vue
@@ -8,7 +8,7 @@
         src="/img/profile/image.webp"
         loading="lazy"
         alt="Profile Picture"
-        sizes="50vw sm:20vw md:400px"
+        sizes="300px sm:300px md:300px"
         height="1586"
         width="1400"
         class="inline-block h-full w-full scale-110 object-contain object-bottom"
